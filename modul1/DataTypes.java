7dbd7aef282003b18e74ba00bb2d803dfbf45773
stwahyuu <septiantw27@gmail.com>
2021-05-30 06:37:05 +0700

    deklarasi variabel dan tipe data

---
+++ modul1/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main (String[] args){
+
+ String namaDepan = "Septian";
+ String namaTengah = "Tri";
+ String namaBelakang = "Wahyudin";
+ int usia = 19;
+ int targetTahunKuliah = 4;
+ double ipk = 3.89764512;
+ char nilaiAbjad = 'A';
+ boolean tampan = true;
+
+ }
+}
